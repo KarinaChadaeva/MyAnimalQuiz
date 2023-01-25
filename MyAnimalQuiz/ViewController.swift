54a28bbcb661d05b42b250b46d79061b264cde88
KarinaChadaeva <118630641+KarinaChadaeva@users.noreply.github.com>
2023-01-25 15:06:30 +0300

    Initial Commit

---
+++ MyAnimalQuiz/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyAnimalQuiz
+//
+//  Created by Карина Чадаева on 25.01.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
