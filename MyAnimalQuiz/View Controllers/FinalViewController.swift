39b9eb38def9f828b669cd2a66613231d6c6bad4
KarinaChadaeva <118630641+KarinaChadaeva@users.noreply.github.com>
2023-01-26 17:43:42 +0300

    All basic elements are added to the storyboard

---
+++ MyAnimalQuiz/View Controllers/FinalViewController.swift
@@ -0,0 +1,17 @@
+//
+//  FinalViewController.swift
+//  MyAnimalQuiz
+//
+//  Created by Карина Чадаева on 26.01.23.
+//
+
+import UIKit
+
+class FinalViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
---
+++ MyAnimalQuiz/View Controllers/QuestionViewController.swift
@@ -0,0 +1,18 @@
+//
+//  QuestionViewController.swift
+//  MyAnimalQuiz
+//
+//  Created by Карина Чадаева on 26.01.23.
+//
+
+import UIKit
+
+class QuestionViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+}
